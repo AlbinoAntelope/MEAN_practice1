e151648a1a7c224dbdb80f9f900ae9e1e54a68cf
AlbinoAntelope <daniel.eryavec@sympatico.ca>
2016-09-09 16:54:20 -0400

    mongo-test

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+var mongojs = require('mongojs');
+var db = mongojs('contactList', ['contactList']);
+
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + "/public"));
@@ -9,26 +13,11 @@ app.use(express.static(__dirname + "/public"));
 app.get('/contactList', function (request, response) {
 	console.log("I received a GET request");
 
-
-    contact1 ={
-    	name:"Tim",
-    	email:"tim@mail.com",
-    	number:"(111) 111-1111"
-    };
-    contact2 ={
-    	name:"David",
-    	email:"dave@mail.com",
-    	number:"(222) 222-2222"
-    };
-    contact3 ={
-    	name:"Emily",
-    	email:"emily@mail.com",
-    	number:"(333) 333-3333"
-    };
-
-    var contactList = [contact1, contact2, contact3];
-    response.json(contactList);
-})
+	db.contactList.find(function (err, docs) {
+		console.log(docs);
+		response.json(docs);
+	});
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
