fa7082897fd20a0b67c95e3d1cdb39755c2c028e
AlbinoAntelope <daniel.eryavec@sympatico.ca>
2016-09-10 15:44:33 -0400

    adding contacts works

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+
+//mongo stuff
 var mongoose = require('mongoose');
 var mongodURI =  'mongodb://heroku_ngrs5bb0:rq4lrabeu1fjmphriqeh37c0g3@ds029106.mlab.com:29106/heroku_ngrs5bb0';
 mongoose.connect(mongodURI);
 var db = mongoose.connection;
-
-
 var ContactSchema = mongoose.Schema({
     name     : String,
     email      : String,
     number      : String
 });
-
 var contactModel = mongoose.model('Contact', ContactSchema);
 
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 //Send Contact List
 app.get('/contactList', function (request, response) {
@@ -28,6 +28,16 @@ app.get('/contactList', function (request, response) {
 		console.log(docs);
 		response.json(docs);
 	});
+});
+
+//Add Contact to Contact List
+app.post('/contactList', function (request, response) {
+	console.log(request.body);
+
+	var newContact = new contactModel(request.body);
+	newContact.save(function (err) {
+		if (err) throw err;
+	})
 });
 
 app.listen(app.get('port'), function() {
--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -7,5 +7,8 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 		$scope.contactList = response;
 	});
 
-    
+   $scope.addContact = function  () {
+		console.log($scope.contact);
+		$http.post('/contactList', $scope.contact);	
+   } 
 }]);
